554e9f45eda9a400773240bae87a79a9b7c05fd8
Yilmaz Karakus <yilmazkarakus14@outlook.com>
2021-10-28 14:55:03 +0100

    Changed jumping and other variables for defensive programming

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,14 +10,16 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 35;
     private float jumpMagnitude = 15;
     private float dashBoost = 5;
+    private float slideBoost = 5;
 
     // Private fields
     private float horizontalValue;
 
     //Booleans
+    private bool isGrounded;
     private bool isJumping;
-    private bool allowDashing;
-    private bool applyJump;
+    private bool isDashing;
+    private bool isSliding;
 
     // gets the animator object
     Animator animator;
@@ -27,7 +29,8 @@ public class PlayerMovement : MonoBehaviour
     const string PLAYER_IDLE = "playerIdle";
     const string PLAYER_RUN = "playerRun";
     const string PLAYER_JUMP = "playerJump";
-
+    const string PLAYER_SLIDE = "playerSlide";
+    const string PLAYER_DASH ="playerDash";
 
 
     // Start is called before the first frame update
@@ -47,22 +50,28 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext value) {
         // The player can only jump, whilst touching the ground.
-        if (value.performed && !isJumping) {
+        if (value.performed && isGrounded) {
             isJumping = true;
-            applyJump = true;
         }
     }
 
     public void Dash(InputAction.CallbackContext value) {
         // The player can only jump, whilst touching the ground.
-        if (value.performed && !allowDashing) {
-            allowDashing = true;
+        if (value.performed && !isDashing) {
+            isDashing = true;
         }
     }
 
+    public void Slide(InputAction.CallbackContext value) {
+        // The player can only jump, whilst touching the ground.
+        if (value.performed && !isSliding) {
+            isSliding = true;
+        }
+    } 
+
     void OnCollisionEnter2D(Collision2D otherCollider){
         if (otherCollider.gameObject.tag == "Floor"){
-            isJumping = false;
+            isGrounded = true;
         }
     }
 
@@ -97,25 +106,26 @@ public class PlayerMovement : MonoBehaviour
         if (horizontalValue != 0f) {
             r2d.AddForce(new Vector2(horizontalValue * speed, 0f), ForceMode2D.Force);
 
-            if (!isJumping){
+            if (isGrounded){
                 changeAnimationState(PLAYER_RUN);
             }
         }
 
         // Jump force is only applied once.
-        if (isJumping && applyJump) {
+        if (isJumping && isGrounded) {
             r2d.AddForce(Vector2.up*jumpMagnitude, ForceMode2D.Impulse);
-            applyJump = false;
+            isGrounded = false;
+            isJumping = false;
             changeAnimationState(PLAYER_JUMP);
         }
 
         // Dash only applied
-        if (allowDashing) {
+        if (isDashing) {
             r2d.AddForce(new Vector2((horizontalValue)*dashBoost,0f), ForceMode2D.Impulse);
-            allowDashing = false;
+            isDashing = false;
         }
 
-        if (horizontalValue==0 && !isJumping){
+        if (horizontalValue==0 && isGrounded){
             changeAnimationState(PLAYER_IDLE);
         }
         
