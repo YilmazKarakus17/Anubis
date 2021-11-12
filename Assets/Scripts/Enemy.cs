0c59d2ddf30e453471ab51c480c03dc5da1d62e5
Ydairz <ydair@hotmail.com>
2021-11-12 01:16:07 +0000

    Player now correctly obtains souls from defeated enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private GameObject player;
     // Combat variables
     public float health = 100;
     public float attackDamage = 25;
@@ -11,16 +12,16 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(float playerAttackDamage) {
         // The enemy takes damage and will die if its health reaches 0.
         health -= playerAttackDamage;
-        Debug.Log(health);
         if (health <= 0) {
             Destroy(gameObject);
+            player.GetComponent<PlayerScript>().addOneSoul();
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
