569612f4ab721e47f813311bf9878d293f21b024
Bilal <bilalkaa@outlook.com>
2021-12-20 19:09:46 +0000

    saving state of difficulty

---
+++ Assets/Scripts/MainMenu/DifficultySelect.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultySelect : MonoBehaviour
+{
+
+    public Button easyBtn;
+    public Button normalBtn;
+    public Button hardBtn;
+
+    public int currentDifficulty;
+
+    // Start is called before the first frame update
+    private void Start()
+    {  
+        currentDifficulty = SaveManager.instance.currentDifficulty;
+
+        if (currentDifficulty==0)
+        {
+            easyBtn.interactable = false;
+            normalBtn.interactable = true;
+            hardBtn.interactable = true;
+        }
+
+        if (currentDifficulty==1)
+        {
+            easyBtn.interactable = true;
+            normalBtn.interactable = false;
+            hardBtn.interactable = true;
+        }
+
+        if (currentDifficulty==2)
+        {
+            easyBtn.interactable = true;
+            normalBtn.interactable = true;
+            hardBtn.interactable = false;
+        }
+        
+        
+    }
+
+
+    public void easyMode()
+    {
+        SaveManager.instance.currentDifficulty = 0;
+        SaveManager.instance.Save();
+
+        if (SaveManager.instance.currentDifficulty==0)
+        {
+            easyBtn.interactable = false;
+            normalBtn.interactable = true;
+            hardBtn.interactable = true;
+        }
+    }
+    public void normalMode()
+    {
+        SaveManager.instance.currentDifficulty = 1;
+        SaveManager.instance.Save();
+        if (SaveManager.instance.currentDifficulty==1)
+        {
+            easyBtn.interactable = true;
+            normalBtn.interactable = false;
+            hardBtn.interactable = true;
+        }
+    }
+    public void hardMode()
+    {
+        SaveManager.instance.currentDifficulty = 2;
+        SaveManager.instance.Save();
+        if (SaveManager.instance.currentDifficulty==2)
+        {
+            easyBtn.interactable = true;
+            normalBtn.interactable = true;
+            hardBtn.interactable = false;
+        }
+    }
+}
