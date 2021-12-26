dfc503d6c1e481fbdf907839e2d40f07f2973456
Yilmaz Karakus <yilmazkarakus14@outlook.com>
2021-12-26 21:32:27 +0000

    Row 3 of level 5 nearly complete

---
+++ Assets/Scripts/Levels/Icicle Level/ChangeMovingCeilingSpeed.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMovingCeilingSpeed : MonoBehaviour
+{
+    [SerializeField] private float speed = 14;
+    [SerializeField] private float exitSpeed = 3;
+    [SerializeField] private GameObject tileMap;
+    [SerializeField] private bool stopPlatformMoving = false;
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.tag == "Player"){
+            this.tileMap.GetComponent<MovingCeiling>().changeSpeed(this.speed);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        if (other.gameObject.tag == "Player"){
+            this.tileMap.GetComponent<MovingCeiling>().changeSpeed(this.exitSpeed);
+            if (this.stopPlatformMoving){
+                this.tileMap.GetComponent<MovingCeiling>().stopMovingTrue();
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/Levels/Icicle Level/MovingCeiling.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCeiling : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private int startingPoint;
+    [SerializeField] private Transform[] points;
+
+    private bool stopMoving;
+
+    private int i; // index of an array
+
+    //To Edit the speed
+    public void changeSpeed(float Speed){
+        this.speed = Speed;
+    }
+
+    public void stopMovingTrue(){
+        this.stopMoving = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = points[startingPoint].position;
+        this.stopMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++;
+            if (i==points.Length)
+            {
+                i=0;
+            }
+        }
+        
+        if (!this.stopMoving){
+            transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        }
+        else{
+            transform.position = Vector2.MoveTowards(transform.position, points[1].position, speed * Time.deltaTime);
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.position.y > transform.position.y)
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.transform.SetParent(null);
+    }
+}
