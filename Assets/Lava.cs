6fb42054c81c9c65cde21ccc4162f5621ad873cf
Ydairz <ydair@hotmail.com>
2021-12-28 12:11:32 +0000

    New key bindings, lava bug fixed and extensions to level 7

--- Assets/Lava.cs
+++ Assets/Lava.cs
@@ -8,15 +8,23 @@ public class Lava : MonoBehaviour
     void OnTriggerEnter2D(Collider2D otherCollider) {
         // Layer 11 is the player layer. Lava will instantly kll player.
         if (otherCollider.gameObject.layer == 11) {
+            float direction;
+            if (otherCollider.gameObject.transform.localScale.y >= 0) {
+                direction = 1;
+            }
+            else {
+                direction = -1;
+            }
+
             otherCollider.GetComponent<Player>().decreaseHealthByPoint(100);
 
             Vector3 velocity = otherCollider.GetComponent<Rigidbody2D>().velocity;
             velocity.x = 0;
-            velocity.y = -1;
+            velocity.y = -1 * direction;
             otherCollider.GetComponent<Rigidbody2D>().velocity = velocity;
 
             
-            otherCollider.GetComponent<Rigidbody2D>().gravityScale = -0.025f;
+            otherCollider.GetComponent<Rigidbody2D>().gravityScale = -0.025f * direction;
         }
     }
 
