982cebbec477c230703d1f899454301754d9b100
Yilmaz Karakus <yilmazkarakus14@outlook.com>
2022-01-13 22:14:24 +0000

    Added coin sound

--- Assets/Scripts/Pickups/Coin.cs
+++ Assets/Scripts/Pickups/Coin.cs
@@ -4,24 +4,36 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    //Sound variables
+    public AudioSource coinSound;
+    private bool allowIncrement;
 
     //If a player is hits the spike initially a lot of hp is taken away
     void OnTriggerEnter2D(Collider2D other){
-        if (other.gameObject.tag == "Player"){
+        if (other.gameObject.tag == "Player" && this.allowIncrement){
             other.gameObject.GetComponent<Player>().incrementCoinCount();
-            Destroy(gameObject);
+            StartCoroutine(destroyCoin());
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.allowIncrement = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Coroutine for the waiting to play out the death of Flying Eye
+    IEnumerator destroyCoin() {
+        this.allowIncrement = false;
+        GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,0f);
+        this.coinSound.Play();
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
     }
 }
