194c4c8bc648db0b1e35e2ee97468e86310566c4
Bilal <bilalkaa@outlook.com>
2022-01-13 15:22:40 +0000

    3D Sound with range, Added new sounds, Added Checkpoints, Linked level 9 with end credits

--- Assets/Scripts/FinishLevel/FinishLevel.cs
+++ Assets/Scripts/FinishLevel/FinishLevel.cs
@@ -28,104 +28,105 @@ public class FinishLevel : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name == "Level9")
         {
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene("EndCredits");
             // if on the final level, redirect to the appropriate screen.
         }
 
+        else{ 
+            if (SceneManager.GetActiveScene().name == "Level1")
+            {
+                if (SaveManager.instance.levelsComplete < 1) {
+                    SaveManager.instance.levelsComplete = 1;
+                }
 
-        if (SceneManager.GetActiveScene().name == "Level1")
-        {
-            if (SaveManager.instance.levelsComplete < 1) {
-                SaveManager.instance.levelsComplete = 1;
+                SaveManager.instance.level1CheckpointX = 0;
+                SaveManager.instance.level1CheckpointY = 0;
+                SaveManager.instance.Save();
+                
             }
 
-            SaveManager.instance.level1CheckpointX = 0;
-            SaveManager.instance.level1CheckpointY = 0;
-            SaveManager.instance.Save();
-            
-        }
-
-        if (SceneManager.GetActiveScene().name == "Level2")
-        {
-            if (SaveManager.instance.levelsComplete < 2) {
-                SaveManager.instance.levelsComplete = 2;
+            if (SceneManager.GetActiveScene().name == "Level2")
+            {
+                if (SaveManager.instance.levelsComplete < 2) {
+                    SaveManager.instance.levelsComplete = 2;
+                }
+                SaveManager.instance.level2CheckpointX = 0;
+                SaveManager.instance.level2CheckpointY = 0;
+                SaveManager.instance.Save();
             }
-            SaveManager.instance.level2CheckpointX = 0;
-            SaveManager.instance.level2CheckpointY = 0;
-            SaveManager.instance.Save();
-        }
 
-        if (SceneManager.GetActiveScene().name == "Level3")
-        {
-            if (SaveManager.instance.levelsComplete < 3) {
-                SaveManager.instance.levelsComplete = 3;
+            if (SceneManager.GetActiveScene().name == "Level3")
+            {
+                if (SaveManager.instance.levelsComplete < 3) {
+                    SaveManager.instance.levelsComplete = 3;
+                }
+                SaveManager.instance.level3CheckpointX = 0;
+                SaveManager.instance.level3CheckpointY = 0;
+                SaveManager.instance.Save();
             }
-            SaveManager.instance.level3CheckpointX = 0;
-            SaveManager.instance.level3CheckpointY = 0;
-            SaveManager.instance.Save();
-        }
 
-        if (SceneManager.GetActiveScene().name == "Level4")
-        {
-            if (SaveManager.instance.levelsComplete < 4) {
-                SaveManager.instance.levelsComplete = 4;
+            if (SceneManager.GetActiveScene().name == "Level4")
+            {
+                if (SaveManager.instance.levelsComplete < 4) {
+                    SaveManager.instance.levelsComplete = 4;
+                }
+                SaveManager.instance.level4CheckpointX = 0;
+                SaveManager.instance.level4CheckpointY = 0;
+                SaveManager.instance.Save();
             }
-            SaveManager.instance.level4CheckpointX = 0;
-            SaveManager.instance.level4CheckpointY = 0;
-            SaveManager.instance.Save();
-        }
 
-        if (SceneManager.GetActiveScene().name == "Level4Cinematic")
-        {
-            if (SaveManager.instance.levelsComplete < 5) {
-                SaveManager.instance.levelsComplete = 5;
+            if (SceneManager.GetActiveScene().name == "Level4Cinematic")
+            {
+                if (SaveManager.instance.levelsComplete < 5) {
+                    SaveManager.instance.levelsComplete = 5;
+                }
             }
-        }
 
-        if (SceneManager.GetActiveScene().name == "Level5")
-        {
-            if (SaveManager.instance.levelsComplete < 6) {
-                SaveManager.instance.levelsComplete = 6;
+            if (SceneManager.GetActiveScene().name == "Level5")
+            {
+                if (SaveManager.instance.levelsComplete < 6) {
+                    SaveManager.instance.levelsComplete = 6;
+                }
+                SaveManager.instance.level5CheckpointX = 0;
+                SaveManager.instance.level5CheckpointY = 0;
+                SaveManager.instance.Save();
             }
-            SaveManager.instance.level5CheckpointX = 0;
-            SaveManager.instance.level5CheckpointY = 0;
-            SaveManager.instance.Save();
-        }
 
-        if (SceneManager.GetActiveScene().name == "Level6")
-        {
-            if (SaveManager.instance.levelsComplete < 7) {
-                SaveManager.instance.levelsComplete = 7;
+            if (SceneManager.GetActiveScene().name == "Level6")
+            {
+                if (SaveManager.instance.levelsComplete < 7) {
+                    SaveManager.instance.levelsComplete = 7;
+                }
+                SaveManager.instance.level6CheckpointX = 0;
+                SaveManager.instance.level6CheckpointY = 0;
+                SaveManager.instance.Save();
             }
-            SaveManager.instance.level6CheckpointX = 0;
-            SaveManager.instance.level6CheckpointY = 0;
-            SaveManager.instance.Save();
-        }
 
-        if (SceneManager.GetActiveScene().name == "Level7")
-        {
-            if (SaveManager.instance.levelsComplete < 8) {
-                SaveManager.instance.levelsComplete = 8;
+            if (SceneManager.GetActiveScene().name == "Level7")
+            {
+                if (SaveManager.instance.levelsComplete < 8) {
+                    SaveManager.instance.levelsComplete = 8;
+                }
+                SaveManager.instance.level7CheckpointX = 0;
+                SaveManager.instance.level7CheckpointY = 0;
+                SaveManager.instance.Save();
             }
-            SaveManager.instance.level7CheckpointX = 0;
-            SaveManager.instance.level7CheckpointY = 0;
-            SaveManager.instance.Save();
-        }
 
-        if (SceneManager.GetActiveScene().name == "Level8")
-        {
-            if (SaveManager.instance.levelsComplete < 9) {
-                SaveManager.instance.levelsComplete = 9;
+            if (SceneManager.GetActiveScene().name == "Level8")
+            {
+                if (SaveManager.instance.levelsComplete < 9) {
+                    SaveManager.instance.levelsComplete = 9;
+                }
+                SaveManager.instance.level8CheckpointX = 0;
+                SaveManager.instance.level8CheckpointY = 0;
+                SaveManager.instance.Save();
             }
-            SaveManager.instance.level8CheckpointX = 0;
-            SaveManager.instance.level8CheckpointY = 0;
-            SaveManager.instance.Save();
+
+
+
+
+            SceneManager.LoadScene("LevelSelect");
         }
-
-
-
-
-        SceneManager.LoadScene("LevelSelect");
 
 
         
